b06ff7032c67443eef15a8daf17283d0e7c7ec45
jay <jay.mm.message@gmail.com>
2021-10-13 00:22:15 +0800

    To maintain the original order of a list but present it in a sorted order, you can use the sorted() function.

---
+++ ch03/cars2.py
@@ -0,0 +1,14 @@
+cars = []
+cars.append('bmw')
+cars.append('audi')
+cars.append('toyota')
+cars.append('subaru')
+
+print("Here is the original list:")
+print(cars)
+
+print("\nHere is the sorted list:")
+print(sorted(cars))
+
+print("\nHere is the original list:")
+print(cars)
