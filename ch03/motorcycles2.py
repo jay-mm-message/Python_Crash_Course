c418047d21150cc85b5f54735d9eba141e7c4885
jay <jay.mm.message@gmail.com>
2021-10-12 19:48:01 +0800

    Motocycles in the list are stored in chronological order according to when we owned them.

--- ch03/motorcycles2.py
+++ ch03/motorcycles2.py
@@ -7,3 +7,7 @@ print(motocycles)
 pop_motocycle = motocycles.pop()
 print(motocycles)
 print(pop_motocycle)
+
+motocycles = ['honda', 'yamaha', 'suzuki']
+last_owned = motocycles.pop()
+print(f"The last motocycle I owned was a {last_owned.title()}")
