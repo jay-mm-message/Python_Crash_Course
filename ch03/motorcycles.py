4becfc9527e255a7446789ee166023d7994029b8
jay <jay.mm.message@gmail.com>
2021-10-12 15:50:43 +0800

    Append an item to a list, the new element is added to the end of the list.

--- ch03/motorcycles.py
+++ ch03/motorcycles.py
@@ -1,5 +1,11 @@
 motocycles = ['honda', 'yamaha', 'suzuki']
 print(motocycles)
 
-motocycles[0] = ['ducati']
+motocycles[0] = 'ducati'
+print(motocycles)
+
+motocycles[0] = 'honda'
+print(motocycles)
+
+motocycles.append('ducati')
 print(motocycles)
