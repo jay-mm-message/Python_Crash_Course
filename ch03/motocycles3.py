5cd28cb692aa070f03f3d07bc12d76c7f1ac80f3
jay <jay.mm.message@gmail.com>
2021-10-12 20:10:18 +0800

    Can use pop() to remove an item from any position in a list by inclucding the index of the item you want to remove in parentheses.

---
+++ ch03/motocycles3.py
@@ -0,0 +1,8 @@
+motocycles = []
+motocycles.append('honda')
+motocycles.append('yamaha')
+motocycles.append('suzuki')
+
+first_owned = motocycles.pop(0)
+message = "The motocycle I owned was a"
+print(f"{message} {first_owned.title()}.")
