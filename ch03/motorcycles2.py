6917769ac5d0408e6edd5681a369b634a35dd361
jay <jay.mm.message@gmail.com>
2021-10-12 19:36:40 +0800

    Pop a value from the list and store that value in the variable poped_motocycle.

---
+++ ch03/motorcycles2.py
@@ -0,0 +1,9 @@
+motocycles = []
+motocycles.append('honda')
+motocycles.append('yamaha')
+motocycles.append('suzuki')
+
+print(motocycles)
+pop_motocycle = motocycles.pop()
+print(motocycles)
+print(pop_motocycle)
